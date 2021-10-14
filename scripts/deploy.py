9f20a05ae8934bcbe481916eb503f4bd042a57ec
wc117 <williamchang89117@gmail.com>
2021-10-15 01:33:32 +0800

    Update deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,8 +3,8 @@ from brownie import aavecellar, accounts
 
 def main():
     acct = accounts.load("deployer_account")
-    name = "Cellar Pool Share Test ETH USDT"
-    symbol = "CPST"
+    name = "AAVE Cellar Pool Share Token"
+    symbol = "ACS"
     lendingPool = "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9"
-    u_token = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    u_token = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
     aavecellar.deploy(name, symbol, lendingPool, u_token, {"from":acct})
